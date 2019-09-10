306bb474d5e04bd20729836fc737dcfc52481d6c
Kathryn Rhoads <46656679+rhokath@users.noreply.github.com>
2019-09-10 17:22:09 -0400

    mvp plus stretch

---
+++ data/migrations/20190910160242_bootstrap.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments();
+        tbl.string('VIN', 17).unique().notNullable();
+        tbl.string('make').notNullable();
+        tbl.string('model').notNullable();
+        tbl.integer('mileage').notNullable();
+        tbl.string('transmissionType');
+        tbl.string('statusType');
+
+    })
+  
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('cars');
+  
+};
